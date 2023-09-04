ee34ee4e552e8b029c38679b5c4c1fe151a8af04
S.Atsushi <atusoccer@gmail.com>
2023-09-04 22:20:47 +0900

    以前のマイグレーションファイル削除

--- db/migrate/20230902124443_create_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateUsers < ActiveRecord::Migration[7.0]
-  def change
-    create_table :users, comment: 'ユーザー情報' do |t|
-      t.string :name, null: false, comment: 'お名前'
-
-      t.timestamps
-    end
-  end
-end
