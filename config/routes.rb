f54f8b59773d5c67516aaf4cb4f42151c1cb4f9b
S.Atsushi <atusoccer@gmail.com>
2023-09-08 01:42:52 +0900

    routes:confirm complete追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :plactices
-    
+  resources :plactices do
+    collection do
+      post :confirm
+      get :complete
+    end
+  end  
 end
