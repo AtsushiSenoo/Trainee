7d6100373e293cdd3073fd57a9df3c146e08ba24
S.Atsushi <atusoccer@gmail.com>
2023-09-18 23:11:35 +0900

    add device gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_06_135604) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_16_092224) do
   create_table "plactices", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", comment: "練習メモ", force: :cascade do |t|
     t.string "name", null: false, comment: "練習名"
     t.integer "weight", comment: "重量"
@@ -19,10 +19,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_06_135604) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", comment: "ユーザー情報", force: :cascade do |t|
-    t.string "name", null: false, comment: "お名前"
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
